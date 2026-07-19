a8adc2aec6f6bfb73fd9a5418f610b1ff3d57be0
shirishapoornam <shirishapoornam@gmail.com>
2026-07-19 18:53:34 +0530

    DAY-1 python basics

---
+++ DAY-1/day1.py
@@ -0,0 +1,41 @@
+server1 = "prod-api-01"
+cpu1 = 72
+
+server2 = "prod-api-02"
+cpu2 = 84
+
+server3 = "prod-api-03"
+cpu3 = 45
+
+
+print("================================")
+print("server1 health")
+print("================================")
+
+
+print("server1:", server1)
+print("cpu1:", cpu1, "%")
+
+
+print("================================")
+print("server2 health")
+print("=================================")
+
+
+print("server2:", server2)
+print("cpu2", cpu2, "%")
+
+
+print("===================================")
+print("server3 health")
+print("====================================")
+
+
+print("server3", server3)
+print("cpu3:", cpu3, "%")
+
+
+print("================================")
+print("overall status: healthy")
+
+
