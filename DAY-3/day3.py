fcbfbe5701da3ed159576732419bb8c99f1ad055
shirishapoornam <shirishapoornam@gmail.com>
2026-07-21 15:07:57 +0530

    Day 3: Learned lists, for loops and enumerate in Python

---
+++ DAY-3/day3.py
@@ -0,0 +1,15 @@
+servers = ["prod-api-01","prod-api-02","prod-db-01"]
+
+for number, server in enumerate(servers, start=1):
+    print("checking server:", number, server)
+
+
+#checking server and list servers added lists here as server
+#checking server1, server2, server3 added number and enumerate here
+#now checking server1,server2,server3 and it should start with 1 not 0 then start=1 or just 1 here
+
+
+
+
+
+
