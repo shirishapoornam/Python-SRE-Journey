fe4ec2be6172b5d3ff0117e152193e3bd29a4c1f
shirishapoornam <shirishapoornam@gmail.com>
2026-07-23 02:14:17 +0530

    Day 4: Learned Python functions, parameters, return values, and reusable server health checker

--- DAY-4/day4.py
+++ DAY-4/day4.py
@@ -8,3 +8,7 @@ def check_server(server):
 for server in servers:
     check_server(server)
 
+#def - define a function
+#function() - calling a function
+#parameter - value which is present inside a function
+#return - return a values instead of saying it gives a value back
